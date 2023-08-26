e9bbe235fe5ca1fc33901fc9477d60edfb71be71
Erik Bernhardsson <mail@erikbern.com>
2023-08-26 16:43:58 -0400

    docs

---
+++ README.md
@@ -0,0 +1,11 @@
+Example of a multi-file Modal project.
+
+Run this using:
+```
+modal run pkg.app --x 4242
+```
+
+Or serve:
+```
+modal serve pkg.app
+```
--- pkg/app.py
+++ pkg/app.py
@@ -1,11 +1,18 @@
-from modal import Stub
+from modal import Stub, web_endpoint
 
 from .lib import f
 
 stub = Stub()
 
 @stub.function()
-def run(x: int):
+@web_endpoint()
+def web(x: int):
     res = f(x)
     print("res:", res)
+    return res
 
+
+@stub.function()
+def fun(x: int):
+    res = f(x)
+    print("res:", res)
